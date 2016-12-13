8dbd12be00c8f059227ba86e8fd3501bb6610dd9
Ravi Chaudhary <rchaudhary@expedia.com>
2016-12-13 19:01:33 +0530

    added router functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from './App';
-
+import Login from './Components/Login';
 ReactDOM.render(
-  <div>
-  <App />
-  </div>,
+  <Router history={browserHistory}>
+    <Route path={"/"} component={App}>
+      <IndexRoute component={App}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
